9479beecda5deb77abc2c4761af3149c6b1828e9
leeenx <570851835@qq.com>
2015-10-02 17:29:34 +0800

    modify

--- numberscroller.js
+++ numberscroller.js
@@ -24,7 +24,8 @@ var numbers=function(_config){
         delay: .3,//组之间的时间延迟，默认.3s
         groupNum:2,//默认2个一组
         diffrentiation:1,//差异化滚动，默认是差异化滚动
-        undreg:'^xxx'//强制无差异化匹配 ^ooxx 表示开头第3，4为无差异化滚动 xxx$ 表示倒数的后三们为无差异滚动
+        minLen:0,//强制数字最小长度，0表示没有限制
+        undreg:''//强制无差异化匹配 ^ooxx 表示开头第3，4为无差异化滚动 xxx$ 表示倒数的后三们为无差异滚动
     },
     scroll=function(){
         var numberContainer=document.querySelectorAll('[HALO-NUMBER-SCROLL]');
@@ -34,10 +35,10 @@ var numbers=function(_config){
         }
     },
     w,h,
-    initNumberScroll=function(container){
+    initNumberScroll=function(container,unscroll){//unscroll参数表示只是显示数字，不滚动
         w=container.offsetWidth,h=container.offsetHeight;
-        var hinput=container.querySelector("input[type=hidden]"),diffrentiation=container.getAttribute("diffrentiation")||config.diffrentiation,undreg=container.getAttribute("undreg")||config.undreg,l2r=container.getAttribute("l2r")||config.l2r;
-        diffrentiation=parseInt(diffrentiation),l2r=parseInt(l2r);
+        var hinput=container.querySelector("input[type=hidden]"),diffrentiation=container.getAttribute("diffrentiation")||config.diffrentiation,undreg=container.getAttribute("undreg")||config.undreg,l2r=container.getAttribute("l2r")||config.l2r,groupNum=container.getAttribute("groupNum")||config.groupNum,minLen=container.getAttribute("minLen")||config.minLen;
+        diffrentiation=parseInt(diffrentiation),l2r=parseInt(l2r),groupNum=parseInt(groupNum)||1,minLen=parseInt(minLen)||0;
         if(!hinput)return ;
         var num=parseInt(hinput.value)||0,_container=container.querySelector("[HALO-NUMBERS-CONTAINER]"),haloNumbers=hinput.getAttribute("HALO-NUMBERS")||"";
         if(num<0){
@@ -51,6 +52,15 @@ var numbers=function(_config){
         }
         if(!num||num==haloNumbers)return ;//不执行
         var ni=(num+'').split(''),len=ni.length;
+        if(minLen){
+            //有最小长度限制
+            if(minLen>len){
+                for(var i=0;i<minLen-len;++i){
+                    ni.unshift(0);
+                }
+                len=minLen;//强制保持一致
+            }
+        }
         if(haloNumbers==""){
             //表示未初始化
             for(var i=0;i<len;++i){
@@ -89,7 +99,7 @@ var numbers=function(_config){
                 //匹配后几位
                 var undregArr=(undreg.replace(/\$$/g,"")).split("");
                 for(var i=len-1;i>=0&&undregArr.length;--i){
-                    if(undregArr.shift()=='x'){
+                    if(undregArr.pop()=='x'){
                         undiffrentiationArr[i]=1;
                     }
                 }
@@ -97,18 +107,18 @@ var numbers=function(_config){
                 undreg='';//如果不是匹配前后数字，则认为为错误匹配直接忽略
             }
         }
-        var perW=w/len,str='',direction=config.direction;
+        var perW=w/len,str='',direction=container.getAttribute("direction")||config.direction;
         if(direction!="up"&&direction!="down")direction="alternate";
         var isAlterNate=(direction=="alternate"),needScrollI=(l2r?0:len);
         for(var i=0;i<len;++i){
-            if(ni[i]==_ni[i]&&diffrentiation&&!undiffrentiationArr[i]){//如果是有差异化滚动就需要走这一分支
+            if(unscroll||ni[i]==_ni[i]&&diffrentiation&&!undiffrentiationArr[i]){//如果是有差异化滚动就需要走这一分支
                 str+='<i style="display:inline-block; vertical-align:top; position:relative; width: '+perW+'px; height: '+h+'px; line-height: '+h+'px; text-align: center; overflow: hidden;">'+ni[i]+'</i>';
             }else{
                 isAlterNate&&(direction=(i%2==0?"up":"down"));
                 if(l2r){//从左到右滚动
-                    str+='<i style="display:inline-block; vertical-align:top; position:relative; width: '+perW+'px; height: '+h+'px; line-height: '+h+'px; text-align: center; overflow: hidden;">'+createNum(ni[i],_ni[i],Math.ceil(needScrollI++/config.groupNum),direction,h)+'</i>';
+                    str+='<i style="display:inline-block; vertical-align:top; position:relative; width: '+perW+'px; height: '+h+'px; line-height: '+h+'px; text-align: center; overflow: hidden;">'+createNum(ni[i],_ni[i],Math.ceil(needScrollI++/groupNum),direction,h)+'</i>';
                 }else{//从右到左滚动
-                    str+='<i style="display:inline-block; vertical-align:top; position:relative; width: '+perW+'px; height: '+h+'px; line-height: '+h+'px; text-align: center; overflow: hidden;">'+createNum(ni[i],_ni[i],Math.ceil(needScrollI--/config.groupNum),direction,h)+'</i>';
+                    str+='<i style="display:inline-block; vertical-align:top; position:relative; width: '+perW+'px; height: '+h+'px; line-height: '+h+'px; text-align: center; overflow: hidden;">'+createNum(ni[i],_ni[i],Math.ceil(needScrollI--/groupNum),direction,h)+'</i>';
                 }
             }
         }
@@ -169,8 +179,16 @@ var numbers=function(_config){
                 config[i]=_config[i];
             }
         }
+    },
+    show=function(){
+        //不滚动，只是显示数字
+        var numberContainer=document.querySelectorAll('[HALO-NUMBER-SCROLL]');
+        for(var i=0,len=numberContainer.length;i<len;++i){
+            numberContainer[i].style.overflow="hidden!important";
+            initNumberScroll(numberContainer[i],true);
+        }
     };
     _config=_config||config;
     set(_config);//默认设置一下配置
-    return {scroll:scroll,set:set}
+    return {scroll:scroll,set:set,show:show}
 };
